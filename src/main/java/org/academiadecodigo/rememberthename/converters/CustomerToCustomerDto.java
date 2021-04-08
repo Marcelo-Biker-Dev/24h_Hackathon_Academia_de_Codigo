ffcd54f9132a17cc0df3f97fa848e6e2df7ec424
Rodrigo Dinis <rodrigodinis20@gmail.com>
2021-04-08 19:35:23 +0000

    more database work in progress

--- src/main/java/org/academiadecodigo/rememberthename/converters/CustomerToCustomerDto.java
+++ src/main/java/org/academiadecodigo/rememberthename/converters/CustomerToCustomerDto.java
@@ -1,4 +1,21 @@
 package org.academiadecodigo.rememberthename.converters;
 
-public class CustomerToCustomerDto {
+import org.academiadecodigo.rememberthename.command.CustomerDto;
+import org.academiadecodigo.rememberthename.persistence.model.Customer;
+
+public class CustomerToCustomerDto extends AbstractConverter<Customer, CustomerDto> {
+
+
+    @Override
+    public CustomerDto convert(Customer customer) {
+
+        CustomerDto customerDto = new CustomerDto();
+        customerDto.setId(customer.getId());
+        customerDto.setFirstName(customer.getFirstName());
+        customerDto.setLastName(customer.getLastName());
+        customerDto.setEmail(customer.getEmail());
+        customerDto.setPhone(customer.getPhone());
+
+        return customerDto;
+    }
 }
--- src/main/java/org/academiadecodigo/rememberthename/converters/ReservationDtoToReservation.java
+++ src/main/java/org/academiadecodigo/rememberthename/converters/ReservationDtoToReservation.java
@@ -1,4 +1,23 @@
 package org.academiadecodigo.rememberthename.converters;
 
-public class ReservationDtoToReservation {
+import org.academiadecodigo.rememberthename.command.ReservationDto;
+import org.academiadecodigo.rememberthename.persistence.model.Reservation;
+import org.academiadecodigo.rememberthename.service.ReservationService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class ReservationDtoToReservation extends AbstractConverter<Reservation, ReservationDto>{
+
+    private ReservationService reservationService;
+
+    @Autowired
+    public void setReservationService(ReservationService reservationService) {
+        this.reservationService = reservationService;
+    }
+
+    @Override
+    public Reservation convert(ReservationDto reservationDto) {
+
+
+    }
+
 }
